d89494dbf2061670b9060466e5f43aada783d0ea
John Balla <john.a.balla@student.uts.edu.au>
2022-10-14 20:29:58 +1100

    removed assignment 3 showcase items such as prefabs, movement script lines, animation cycles. Updated folder locations and animator states for future use.

--- Assets/Scripts/MovementScripts/MovementManager.cs
+++ Assets/Scripts/MovementScripts/MovementManager.cs
@@ -16,14 +16,7 @@ public class MovementManager : MonoBehaviour
     [SerializeField] private AudioClip[] footstepClips;
 
     //vector list: pirate's first iteration movement in top left corner
-    static float west = -6.25f;
-    static float east = -3.75f;
-    static float north = 6.75f;
-    static float south = 4.75f;
-    private Vector3 vectorSW = new(west, south, 0.0f);
-    private Vector3 vectorSE = new(east, south, 0.0f);
-    private Vector3 vectorNE = new(east, north, 0.0f);
-    private Vector3 vectorNW = new(west, north, 0.0f);
+    private Vector2 start = new(-6.25f, 6.75f);
     private const float pirateSpeed = 1.1f;
 
     // Start is called before the first frame update
@@ -32,7 +25,7 @@ public class MovementManager : MonoBehaviour
         animator = pirate.GetComponent<Animator>();
         tweener = GetComponent<WalkTweener>();
 
-        animator.SetTrigger("HitWall"); //initial trigger to start cycle
+        //animator.SetTrigger("HitWall"); //initial trigger to start cycle
 
         lastPos = pirate.transform.position;
         distance = 0.0f;
@@ -41,48 +34,19 @@ public class MovementManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveClockwise();
-
         distance = Vector3.Distance(lastPos, pirate.transform.position);
-        if (distance >= 0.5f)
-        {
-            lastPos = pirate.transform.position;
-            PlayFootstepAudio();
-        }
-
-    }
-
-    private void MoveClockwise()
-    {
-        //move clockwise depending on location over 3 seconds
-        if (pirate.transform.position == vectorSW)
-        {
-            animator.SetTrigger("UpPress");
-            tweener.AddTween(pirate.transform, vectorSW, vectorNW, 1 / pirateSpeed);
-        }
-        else if (pirate.transform.position == vectorNW)
-        {
-            animator.SetTrigger("RightPress");
-            tweener.AddTween(pirate.transform, vectorNW, vectorNE, 1 / pirateSpeed);
-        }
-        else if (pirate.transform.position == vectorNE)
-        {
-            animator.SetTrigger("DownPress");
-            tweener.AddTween(pirate.transform, vectorNE, vectorSE, 1 / pirateSpeed);
-        }
-        else if (pirate.transform.position == vectorSE)
-        {
-            animator.SetTrigger("LeftPress");
-            tweener.AddTween(pirate.transform, vectorSE, vectorSW, 1 / pirateSpeed);
-        }
     }
 
     void PlayFootstepAudio()
     {
-        if (!footstepSource.isPlaying)
+        if (distance >= 0.5f)
         {
-            footstepSource.clip = footstepClips[footstepSource.clip == footstepClips[0] ? 1 : 0]; //alternate between two clips
-            footstepSource.Play();
+            lastPos = pirate.transform.position;
+            if (!footstepSource.isPlaying)
+            {
+                footstepSource.clip = footstepClips[footstepSource.clip == footstepClips[0] ? 1 : 0]; //alternate between two clips
+                footstepSource.Play();
+            }
         }
     }
 }
