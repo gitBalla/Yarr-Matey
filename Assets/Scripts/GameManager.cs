cd4e50386686a865867f712d589128127292b976
John Balla <john.a.balla@student.uts.edu.au>
2022-10-22 20:50:29 +1100

    changes for aspect ratio adaptability, added quit button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,5 +8,10 @@ public class GameManager : MonoBehaviour
     public enum GameState { Start, FirstLevel, SecondLevel }
 
     public static GameState currentGameState = GameState.Start;
+
+    public void QuitGame()
+    {
+        EditorApplication.isPlaying = false;
+    }
 
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,23 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     // Member Variables
-    private GameManager gameManager;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     //on click action for start game, loads first level
     public void ClickStartFirstLevel()
